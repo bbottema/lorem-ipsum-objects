625870f3205c7b9735ba6a02d587637216887ed5
b.bottema@gmail.com <b.bottema@gmail.com@d8bd78e9-456f-c896-a6ce-b8555cb56a9f>
2012-08-08 11:40:37 +0000

    moved some classes around

--- trunk/src/main/java/org/dummycreator/DummyCreator.java
+++ trunk/src/main/java/org/dummycreator/DummyCreator.java
@@ -33,9 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import org.dummycreator.binder.ClassBinder;
-import org.dummycreator.cache.ConstructorCache;
-import org.dummycreator.cache.MethodCache;
 
 /**
  * 
