f2cac1029fa16c59458e027ba7572cc929f1ebc1
bbottema <b.bottema@projectnibble.org>
2012-08-31 10:54:15 +0000

    cleaned up imports

--- src/test/java/org/dummycreator/dummyfactories/RandomPrimitiveFactoryTest.java
+++ src/test/java/org/dummycreator/dummyfactories/RandomPrimitiveFactoryTest.java
@@ -3,7 +3,6 @@ package org.dummycreator.dummyfactories;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.dummycreator.RandomCreator;
 import org.easymock.EasyMock;
